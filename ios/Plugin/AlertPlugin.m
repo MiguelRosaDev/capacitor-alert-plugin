e0df50e96618f2a08f280840fac56eac8046e3c3
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-02-27 11:28:52 +0000

    Update AlertPlugin.m

--- ios/Plugin/AlertPlugin.m
+++ ios/Plugin/AlertPlugin.m
@@ -1,1 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <Capacitor/Capacitor.h>
 
+// Define the plugin using the CAP_PLUGIN Macro, and
+// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
+CAP_PLUGIN(AlertPlugin, "AlertPlugin",
+           CAP_PLUGIN_METHOD(showAlert, CAPPluginReturnPromise);
+)
