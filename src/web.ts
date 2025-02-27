7a1fb5e4ee8d64d51b44cea6c219a89284996c75
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-02-27 11:29:53 +0000

    Update web.ts

--- src/web.ts
+++ src/web.ts
@@ -1,1 +1,8 @@
+import { WebPlugin } from '@capacitor/core';
+import type { AlertPluginPlugin } from './index';
 
+export class AlertPluginWeb extends WebPlugin implements AlertPluginPlugin {
+  async showAlert(options: { message: string }): Promise<void> {
+    window.alert(options.message);
+  }
+}
