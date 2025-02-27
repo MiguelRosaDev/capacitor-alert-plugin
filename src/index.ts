f0600b190289531d70a05a97f57db8940d7f051f
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-02-27 11:29:37 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { registerPlugin } from '@capacitor/core';
 
+export interface AlertPluginPlugin {
+  showAlert(options: { message: string }): Promise<void>;
+}
+
+const AlertPlugin = registerPlugin<AlertPluginPlugin>('AlertPlugin');
+
+export { AlertPlugin };
