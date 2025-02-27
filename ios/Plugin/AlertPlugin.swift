1ab5f3c040c25e747b2cc2013d00853ed7adc4ca
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-02-27 11:28:07 +0000

    Update AlertPlugin.swift

--- ios/Plugin/AlertPlugin.swift
+++ ios/Plugin/AlertPlugin.swift
@@ -1,1 +1,21 @@
+import Foundation
+import Capacitor
 
+@objc(AlertPlugin)
+public class AlertPlugin: CAPPlugin {
+    @objc func showAlert(_ call: CAPPluginCall) {
+        guard let message = call.getString("message") else {
+            call.reject("Message cannot be empty")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                call.resolve()
+            })
+            
+            self.bridge?.viewController?.present(alertController, animated: true)
+        }
+    }
+}
